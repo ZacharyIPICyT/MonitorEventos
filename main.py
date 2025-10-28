56c7ae289572d980a201ac45b1a1baf573c3feda
ZacharyIPICyT <marco.zach.moreno@gmail.com>
2025-10-28 03:41:36 -0600

    Add main function to run VideoEventMonitor

---
+++ main.py
@@ -0,0 +1,9 @@
+from video_event_monitor import VideoEventMonitor
+
+def main():
+    """Punto de entrada principal de la aplicación"""
+    monitor = VideoEventMonitor()
+    monitor.run()
+
+if __name__ == "__main__":
+    main()
